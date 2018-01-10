e31462a2d7b70befaffdf2008013477e8839254f
jpwebdev <jjpwebdev@gmail.com>
2018-01-09 20:53:22 -0500

    Homework completed

--- app.js
+++ app.js
@@ -34,10 +34,15 @@ const gen_display_arr = () => {
 
 const play_now = () => {
     gen_display_arr();  // Generate an array with the corresponding dashes/letters
-    
+    let did_I_just_win = false;
+
+    if (display_arr.join() == word.word_arr.join()) {
+        did_I_just_win = true;
+    }
     console.log(`The Word is: ${word.word}`); // Debugging: Word to be guessed
 
-    if (word.guess > 0) { // User still has guesses remaining
+
+    if (word.guess > 0 && !did_I_just_win) { // User still has guesses remaining
 
         for (let i = 0; i < word.word_length; i++) {   // Display _ in the command prompt
             process.stdout.write(`${display_arr[i]} `); // Console.log creates a new line automatically, process.stdout.write doesn't
@@ -56,28 +61,29 @@ const play_now = () => {
                 word.guess--;   // Decrease guesses
                 console.log(`Guesses reamining: ${word.guess}\n`);
                 
-                if (word.guess < 1) {   // User runs out of tries
-                    console.log("Game Over, you lose!");
-                } 
-                else{   
-                    play_now();
-                }
+                play_now();           
             } 
             //  User guessed right: letter typed IS in the array
             else {
                 console.log("Right");   // Alert the user
 
-                for ( let i = 0; i < word.word_length; i++) {   // Look for the typed word in the array of Letter instances and change the property value
+                for (let i = 0; i < word.word_length; i++) {   // Look for the typed word in the array of Letter instances and change the property value
                     if (word.word_arr[i] == answer.user_guess) {
                         arr_Letters[i].guessed = true;
                     }
                 }
-                
+
                 play_now(); // Play again
             }
 
         });
-    };
+    }
+    else if(word.guess > 0 && did_I_just_win){
+        console.log("\nCongratulations\nYou Won!");
+    }
+    else {
+        console.log("\nGame Over\nYou Lose!");
+    }
         
 }
 
